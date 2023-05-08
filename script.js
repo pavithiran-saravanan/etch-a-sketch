683e6e13b68905d65fc1c414475f8d395b6a3f95
pavithiran <pavithransaravanan22@gmail.com>
2023-05-09 00:27:32 +0530

    Change to purple theme

--- script.js
+++ script.js
@@ -30,9 +30,8 @@ function generateCells(){
             cell.setAttribute('id', `cell-${i}-${j}`);
             cell.style.width = l + 'px';
             cell.style.height = l + 'px';
-            cell.style.border = '1px solid #eee'
-            // cell.style.borderRadius = '5px';
-            cell.style.backgroundColor = '#ddd';
+            cell.style.border = '.5px solid #7784c4'
+            cell.style.backgroundColor = '#fff';
             row.appendChild(cell);
         }
         container.appendChild(row);
@@ -49,7 +48,7 @@ function clearCells(){
 function resetColor(){
     console.log('reset');
     const cells = document.querySelectorAll('.cell');
-    cells.forEach((cell) => {cell.style.backgroundColor = '#ddd'});
+    cells.forEach((cell) => {cell.style.backgroundColor = '#fff'});
 }
 
 // Add eventlisterner to each of the cells present in the container such that they change color on hover
@@ -60,9 +59,9 @@ function addHoverListener(){
             if(rgb) e.target.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()}`;
             else{
                 const color = getComputedStyle(e.target).backgroundColor;
-                const r = color.split(',')[0].split('(')[1] * 0.7;
-                const g = color.split(',')[1] * 0.7;
-                const b = color.split(',')[2].split(')')[0] * 0.7;
+                const r = color.split(',')[0].split('(')[1] * 0.71;
+                const g = color.split(',')[1] * 0.71;
+                const b = color.split(',')[2].split(')')[0] * 0.99;
                 const targetColor = `rgb(${r}, ${g}, ${b})`;
                 e.target.style.backgroundColor = targetColor;
             }
@@ -89,10 +88,10 @@ function rand(){
 const rgbButton = document.querySelector('.rgb');
 function toggleRGB(){
     resetColor();
-    const title  = document.querySelector('.title');
-    title.classList.toggle('title-rgb');
-    const github = document.querySelector('.github-link');
-    github.classList.toggle('github-link-rgb');
+    // const title  = document.querySelector('.title');
+    // title.classList.toggle('title-rgb');
+    // const github = document.querySelector('.github-link');
+    // github.classList.toggle('github-link-rgb');
     rgbButton.classList.toggle('rgb-on');
     rgb = !rgb;
 }
